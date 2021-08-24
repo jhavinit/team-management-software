01320dbe0777790443829d2e23d9128daea0ab26
kaursukhvir <67436472+kaursukhvir@users.noreply.github.com>
2021-08-25 00:20:47 +0530

    added login route

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const authController = require('./../controllers/authController');
+const express = require("express");
+const authController = require("./../controllers/authController");
 const router = express.Router();
 
-router.post('/register', authController.signup);
+router.post("/register", authController.signup);
+router.post("/login", authController.login);
 
 module.exports = router;
